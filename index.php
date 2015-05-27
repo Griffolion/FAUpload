a55b6ae6612734cfba204aa4aca913179a9c7efe
Andy <a_hall83@hotmail.com>
2015-05-27 12:10:14 -0400

    test request to box authentication

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -10,6 +9,13 @@
     </head>
     <body>
         <h1>Form Assembly Box Integration</h1>
+        <form action='https://app.box.com/api/oauth2/authorize' method='POST'>
+            <input type="hidden" name="response_type" value="Code"  />
+            <input type="hidden" name="client_id" value="z4cbqfi5tj82xr0wujz7q4cgi88cs6ag"  />
+            <input type='hidden' name='redirect_uri' value='http://localhost/FAUpload/index.php' />
+            <input type="hidden" name="state" value="secret22" />
+            <input value="Click to link Box to your Form Assembly account" type="submit" class="btn-lg btn btn-success"  />
+        </form>
         <?php
         // put your code here
         ?>
